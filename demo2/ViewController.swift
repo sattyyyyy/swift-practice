6b8a41e0eef3539597356c4c0427ea83d4b03661
Michelle Chen <chenyunchieh@chenteki-macbook-air.local>
2016-10-19 19:18:34 +0800

    Initial Commit

---
+++ demo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo2
+//
+//  Created by Michelle Chen on 2016/10/19.
+//  Copyright © 2016年 Michelle Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
